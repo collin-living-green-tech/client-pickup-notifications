51b8f9ab6cb60655970b2833450a24264c0a0f6a
collin-living-green-tech <collin@livinggreentechnology.org>
2022-06-15 14:34:51 -0700

    Added default html page comingsoon.html

--- apis/views.py
+++ apis/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# a function view to render the
+# comingsoon.html page
+
+def index(request):
+    return render(request,"apis/comingsoon.html")
+
